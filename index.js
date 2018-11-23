0df779549fcbb2ba889c5d9b25a2e6c2e04a1ab5
Bounteous <bounteous17@protonmail.com>
2018-11-23 21:26:39 +0100

    Add express routes

---
+++ index.js
@@ -0,0 +1,33 @@
+const debug = require('debug')('Vril:Index');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const vrilConfig = require('./config/config');
+
+const publicRoutes = require('./src/public/public.routes');
+const usersRoutes = require('./src/users/user.routes');
+
+// Modules
+const vrilDB = require('./src/modules/mongo.module');
+
+app.use(bodyParser.json());
+
+// Cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+})
+
+// Public Routes
+app.use('/', publicRoutes);
+// Protected routes
+app.use('/users', usersRoutes);
+
+// Create express server
+const port = vrilConfig().server.port;
+const host = vrilConfig().server.host;
+app.listen(port, host, async () => {
+    console.log('Vril API Server running on port:', port);
+}); 
