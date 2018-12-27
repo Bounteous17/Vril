efc11dd649152db7347960e140b4a8a051b354f5
Bounteous <bounteous17@protonmail.com>
2018-12-27 19:44:33 +0100

    Change login middle usage

--- src/public/public.module.js
+++ src/public/public.module.js
@@ -8,7 +8,8 @@ const VrilConfig = require("../../config/config")();
 
 const __moduleSettings = () => {
   return {
-    badAuth: { errorCode: 500, message: "No user found or password incorrect" }
+    badAuth: { errorCode: 500, message: "No user found or password incorrect" },
+    userExists: { errorCode: 500, message: "Username already registered" },
   };
 };
 
@@ -17,23 +18,29 @@ const __status = async req => {
   return { status: "alive" };
 };
 
+const __findUser = async (username, values) => {
+  return await Users.findOne({
+    username: username
+  }).select(values);
+}
+
 const __login = async (username, password) => {
   try {
-    let sUser = await Users.findOne({
-      username: username
-    }).select("_id password");
-
+    let sUser = await __findUser(username);
     if (!sUser) {
       return __moduleSettings().badAuth;
     }
+
     let authSuccess = await bcrypt.compare(password, sUser.password);
     if (!authSuccess) {
       return __moduleSettings().badAuth;
     }
+
     let token = await __generateJwt(sUser._id);
     if (!token) {
       return __moduleSettings().badAuth;
     }
+
     return {
       message: "Auth correct",
       token: token
@@ -69,6 +76,7 @@ const __generateJwt = async user => {
 module.exports = () => {
   return {
     status: __status,
+    signup: __signup,
     login: __login
   };
 };
--- src/public/public.routes.js
+++ src/public/public.routes.js
@@ -1,36 +1,36 @@
 const debug = require("debug")("Vril:PublicRouter");
 const sanitize = require("mongo-sanitize");
 const express = require("express"),
-  router = express.Router();
+    router = express.Router();
 const PublicModule = require("./public.module")();
 const Helpers = require("./../helpers/helpers")();
 const FieldsValid = require("../@middlewares/fields.middleware");
 
 router.get("/", async (req, res) => {
-  try {
-    const status = await PublicModule.status(req);
-    res.send(status);
-  } catch (error) {
-    debug(error);
-    res.status(500);
-    res.send({ status: "failed" });
-  }
+    try {
+        const status = await PublicModule.status(req);
+        res.send(status);
+    } catch (error) {
+        debug(error);
+        res.status(500);
+        res.send({ status: "failed" });
+    }
 });
 
-router.post("/login", FieldsValid, async (req, res) => {
-  try {
-    let username = sanitize(req.body.Username);
-    let password = sanitize(req.body.Password);
-    let resLogin = await PublicModule.login(username, password);
-    let { status, body } = Helpers.resStatGen(resLogin);
-    res.status(status);
-    res.send(body);
-    return;
-  } catch (error) {
-    debug(error);
-    res.status(500);
-    res.send({ status: "failed" });
-  }
+router.post("/login", FieldsValid('login'), async (req, res) => {
+    try {
+        let username = sanitize(req.body.Username);
+        let password = sanitize(req.body.Password);
+        let resLogin = await PublicModule.login(username, password);
+        let { status, body } = Helpers.resStatGen(resLogin);
+        res.status(status);
+        res.send(body);
+        return;
+    } catch (error) {
+        debug('Error into /user/login: %o', error);
+        res.status(500);
+        res.send("Login server error");
+    }
 });
 
 module.exports = router;
