6b86433ce142d5de6fe92a81f8d1d9c46979b785
Bounteous <bounteous17@protonmail.com>
2018-12-27 20:39:02 +0100

    Added signup fields validation

--- src/@middlewares/fields.middleware.js
+++ src/@middlewares/fields.middleware.js
@@ -6,29 +6,31 @@ const Helpers = require("../helpers/helpers")();
 
 module.exports = (resource) => {
   return (req, res, next) => {
+    let validFields = {};
     if (resource === 'login') {
-      try {
-        let validFields = Helpers.fieldsCheck(
-          [req.body.Username, req.body.Password],
-          "user and password"
-        );
-        if (!validFields.error) {
-          return next();
-        } else {
-          res.status(validFields.status);
-          res.send(validFields.message);
-          return;
-        }
-      } catch (error) {
-        res.status(500);
-        res.send("Internal error");
-        return;
-      }
+      validFields = Helpers.fieldsCheck(
+        [req.body.Username, req.body.Password],
+        "user and password"
+      );
+    } else if (resource === 'signup') {
+      validFields = Helpers.fieldsCheck(
+        [req.body.Username, req.body.Password, req.body.Mail],
+        "user, password and email"
+      );
     } else {
-      debug('Error on __fieldsValidator: %o', 'Not a valid resource')
+      debug('Error on __fieldsValidator: %o', new Error('Not a valid resource'))
       res.status(500);
       res.send('Internal error');
       return;
     }
+
+    if (!validFields.error) {
+      return next();
+    } else {
+      res.status(validFields.status);
+      res.send(validFields.message);
+      return;
+    }
+
   };
 }
